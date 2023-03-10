d1c3d6b5571a02c1fe23ad7e322c60610f74c606
Nicolas Garay Grau <nicolasgaray.2295@gmail.com>
2023-03-10 05:38:10 -0300

    Arreglos

--- Logica.js
+++ Logica.js
@@ -1,45 +1,39 @@
 
-// 
-// const button = document.querySelector("btn");
-// const caja = document.getElementById('contenedor');
+const button = document.querySelector("btn");
+const caja = document.getElementById('contenedor');
 
 
+button.addEventListener ("click", idioma);
+caja.addEventListener ("click", moscuro);
+
+function idioma (){
+     document.getElementById("mensaje").innerText = "Hello! I am Jose Hernandez, I am 27 years old and I am a Programmer. I have little experience in companies but I have worked on many projects individually. Now I am looking for a job in a company that allows me to grow professionally and personally. Greetings!";
+     document.getElementById("datosp").innerText = "Personal data";
+     document.getElementById("datoshab").innerText = "Skills";
+     document.getElementById("datosexp").innerText = "Work experience";
+     document.getElementById("name").innerText = "Full name: Jose Hernandez";
+     document.getElementById("date").innerText = "Date of birth: 07.22.95";
+     document.getElementById("id").innerText = "ID document: 25698741";
+     document.getElementById("tel").innerText = "Mobile: 381654979";
+     document.getElementById("mail").innerText = "E-mail: jos_h85@gmail.com";
+     document.getElementById("adress").innerText = "Adress: Piedra buena 332 Street";
+     document.getElementById("status").innerText = "Martial status: Single";
+     document.getElementById("place").innerText = "Place of birth: Argentina";
+     document.getElementById("mleng").innerText = "Language management:  SQL, PYTHON y JAVASCRIPT";
+     document.getElementById("mapp").innerText = "Application management:  WORD, EXCEL y POWER POINT";
+     document.getElementById("mred").innerText = "Social media management:  Facebook e Instagram";
+     document.getElementById("exp1").innerText = "Mercado Libre: Developer (may 2020 - july 2021)";
+     document.getElementById("exp2").innerText = "Freelance: Design of web pages and applications (august 2021 - present)";
+
+    }
 
 
-
-// button.addEventListener ("click", idioma);
-// caja.addEventListener ("click", moscuro);
-
-// function idioma (){
-//     document.getElementById("mensaje").innerText = "Hello! I am Jose Hernandez, I am 27 years old and I am a Programmer. I have little experience in companies but I have worked on many projects individually. Now I am looking for a job in a company that allows me to grow professionally and personally. Greetings!";
-//     document.getElementById("datosp").innerText = "Personal data";
-//     document.getElementById("datoshab").innerText = "Skills";
-//     document.getElementById("datosexp").innerText = "Work experience";
-//     document.getElementById("name").innerText = "Full name: Jose Hernandez";
-//     document.getElementById("date").innerText = "Date of birth: 07.22.95";
-//     document.getElementById("id").innerText = "ID document: 25698741";
-//     document.getElementById("tel").innerText = "Mobile: 381654979";
-//     document.getElementById("mail").innerText = "E-mail: jos_h85@gmail.com";
-//     document.getElementById("adress").innerText = "Adress: Piedra buena 332 Street";
-//     document.getElementById("status").innerText = "Martial status: Single";
-//     document.getElementById("place").innerText = "Place of birth: Argentina";
-//     document.getElementById("mleng").innerText = "Language management:  SQL, PYTHON y JAVASCRIPT";
-//     document.getElementById("mapp").innerText = "Application management:  WORD, EXCEL y POWER POINT";
-//     document.getElementById("mred").innerText = "Social media management:  Facebook e Instagram";
-//     document.getElementById("exp1").innerText = "Mercado Libre: Developer (may 2020 - july 2021)";
-//     document.getElementById("exp2").innerText = "Freelance: Design of web pages and applications (august 2021 - present)";
-
-// }
-
-
-// // function moscuro () {
-//     document.getElementById("contenedor").style.backgroundColor = "#000";
-//     document.getElementById("body").style.backgroundColor = "white";
-//     document.getElementById("oscuro").innerText = "Modo claro";
-//     document.getElementById("perfil").style.border = "3px solid white";
-//     // document.getElementsByClass("boton").style = "1px white solid";
-//     const collection = document.getElementsByClassName("boton");
-//     collection[1].style.border = "#999";}
+function moscuro () {
+     document.getElementById("contenedor").style.backgroundColor = "#000";
+     document.getElementById("body").style.backgroundColor = "white";
+     document.getElementById("oscuro").innerText = "Modo claro";
+     document.getElementById("perfil").style.border = "3px solid white";
+    }
 
 
 
